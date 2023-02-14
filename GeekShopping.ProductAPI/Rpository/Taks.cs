d68829bb6e9e7cb89f6e52c3c7dd3197f2993e58
Lorrayne Rodrigues <lorrayne.rodrigues@animaeducacao.com.br>
2023-02-14 15:18:59 -0300

    Primeiro commit com arquivos

---
+++ GeekShopping.ProductAPI/Rpository/Taks.cs
@@ -0,0 +1,6 @@
+﻿namespace GeekShopping.ProductAPI.Rpository
+{
+    public class Taks<T>
+    {
+    }
+}
---
+++ GeekShoppingWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GeekShoppingWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
