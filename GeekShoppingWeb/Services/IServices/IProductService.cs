229d80bf60e88a14f60504bcde4e3d7e6508c8ab
Lorrayne Rodrigues <lorrayne.rodrigues@animaeducacao.com.br>
2023-03-01 14:25:21 -0300

    [Preparando web para consumir a api de produto

---
+++ GeekShoppingWeb/Services/IServices/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using GeekShoppingWeb.Models;
+
+namespace GeekShoppingWeb.Services.IServices
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductModel>> FindAllProducts();
+        Task<ProductModel> FindProductById(long id);
+        Task<ProductModel> CreateProduct(ProductModel model);
+        Task<ProductModel> UpdateProduct(ProductModel model);
+        Task<bool> DeleteProductById(long id);
+    }
+}
